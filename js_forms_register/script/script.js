b99d6da4dee3c0982dce3673d003a59191401bea
Aleksandar Todorovic <aleksandar.todorovic@utb.ecutbildning.se>
2021-10-30 02:10:09 +0200

    test-2

--- js_forms_register/script/script.js
+++ js_forms_register/script/script.js
@@ -1,3 +1,4 @@
+//VARIABLER HTML ELEMENT
 const form = document.getElementById('form')
 const firstName = document.getElementById('firstname')
 const lastName = document.getElementById('lastname')
@@ -9,14 +10,16 @@ const address = document.getElementById('address')
 const zipCode = document.getElementById('zip_code')
 const submit = document.getElementById('submit')
 
+//ARRAY FÖR HTML VARIABLER
 let form_value_array = [firstName, lastName, email, password, confirmPassword, dateOfBirth, address, zipCode]
 
-function validate_length(element, min_length) {
+//VALIDERAR LÄNGD MED REGULAR EXPRESSION
+function check_length(element) {
+    const regEx_length = /^([A-Za-z]).{1,}$/;
     const form_manager = element.parentElement
     const error_text = form_manager.querySelector('small')
-
-    if(element.value.length < min_length) {
-        error_text.innerText = `You need at least ${min_length} characters`
+    if(!regEx_length.test(element.value)) {
+        error_text.innerText = `You need at least 2 letters.`
         form_manager.className = 'form_manager border_warning'
     } else {
         error_text.innerText = ``
@@ -24,12 +27,13 @@ function validate_length(element, min_length) {
     }
 }
 
-function check_password(element) {
-    const regEx_password = /^(?=(.*[a-z]){3,})(?=(.*[A-Z]){2,}).{8,}$/;
+//VALIDERAR EXAKT LÄNGD MED REGULAR EXPRESSION
+function check_exact(element) {
+    const regEx_exact = /^([0-9]{5})$/;
     const form_manager = element.parentElement
     const error_text = form_manager.querySelector('small')
-    if(!regEx_password.test(element.value)) {
-        error_text.innerText = `Min req. 3-lowercase 2-uppercase 2-numbers`
+    if(!regEx_exact.test(element.value)) {
+        error_text.innerText = `You need exactly 5 numbers, only numbers.`
         form_manager.className = 'form_manager border_warning'
     } else {
         error_text.innerText = ``
@@ -37,28 +41,13 @@ function check_password(element) {
     }
 }
 
-function more_or_less(element, min_length) {
-    const form_manager = element.parentElement
-    const error_text = form_manager.querySelector('small')
-
-    if(element.value.length < min_length) {
-        error_text.innerText = `You need exactly ${min_length} characters`
-        form_manager.className = 'form_manager border_warning'
-    } else if(element.value.length > min_length) {
-        error_text.innerText = `You need exactly ${min_length} characters`
-        form_manager.className = 'form_manager border_warning'
-    } else {
-        error_text.innerText = ``
-            form_manager.className = 'form_manager'
-    }
-}
-
+//VALIDERAR EMAIL ADRESSER MED REGULAR EXPRESSION. MIN KRAV EX: a@a.se
 function check_email(element) {
     const regEx_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const form_manager = element.parentElement
     const error_text = form_manager.querySelector('small')
     if(!regEx_email.test(element.value)) {
-        error_text.innerText = `You need a valid email address`
+        error_text.innerText = `You need a valid email address.`
         form_manager.className = 'form_manager border_warning'
     } else {
         error_text.innerText = ``
@@ -66,6 +55,21 @@ function check_email(element) {
     }
 }
 
+//VALIDERAR LÖSENORD MED REGULAR EXPRESSION. KRAV PÅ 8 TECKEN - 3 LOWERCASE 2 UPPERCASE 2 SIFFROR 
+function check_password(element) {
+    const regEx_password = /^(?=(.*[a-z]){3,})(?=(.*[A-Z]){2,})(?=(.*[0-9]){2,}).{8,}$/;
+    const form_manager = element.parentElement
+    const error_text = form_manager.querySelector('small')
+    if(!regEx_password.test(element.value)) {
+        error_text.innerText = `Min req.(8-char) 3-lowercase 2-uppercase 2-numbers.`
+        form_manager.className = 'form_manager border_warning'
+    } else {
+        error_text.innerText = ``
+        form_manager.className = 'form_manager'
+    }
+}
+
+//KONTROLLERAR CONFIRM PASSWORD SÅ ATT DET STÄMMER ÖVERENS MED PASSWORD
 function check_confirmed_password(key, key2) {
     const form_manager = confirmPassword.parentElement
     const error_text = form_manager.querySelector('small')
@@ -79,6 +83,8 @@ function check_confirmed_password(key, key2) {
     }
 }
 
+//KONTROLL AV INPUT. DRAR AV {DATE OF BIRTH} FRÅN DAGENS DATUM FÖR ATT VALIDERA OM DU ÄR 18 ÅR 
+//(GRATULERAR DIG PÅ FÖDELSEDAGEN OM DU FYLLER ÅR IDAG.)
 function check_date(element) {
     const form_manager = element.parentElement
     const error_text = form_manager.querySelector('small')
@@ -105,11 +111,12 @@ function check_date(element) {
     }
 }
 
+//KONTROLLERAR ALLA FÄLT FÖRUTOM {DATE OF BIRTH} PÅ KEY UP
 firstName.addEventListener('keyup', function(e) {
-    validate_length(e.target, 2)
+    check_length(e.target)
 })
 lastName.addEventListener('keyup', function(e) {
-    validate_length(e.target, 2)
+    check_length(e.target)
 })
 email.addEventListener('keyup', function(e) {
     check_email(e.target)
@@ -121,12 +128,13 @@ confirmPassword.addEventListener('keyup', function(e) {
     check_confirmed_password(confirmPassword.value, password.value)
 })
 address.addEventListener('keyup', function(e) {
-    validate_length(e.target, 2)    
+    check_length(e.target, 2)    
 })
 zipCode.addEventListener('keyup', function(e) {
-    more_or_less(e.target, 5)
+    check_exact(e.target)
 })
 
+//KONTROLLERAR ALLA FÄLT VID SUBMIT
 form.addEventListener('submit', function(e) {
 
     e.preventDefault()
@@ -134,11 +142,10 @@ form.addEventListener('submit', function(e) {
         switch(element.id) {
             
             case 'firstname':
-                validate_length(element, 2)
-                console.log(element.id)
+                check_length(element)
                 break;
             case 'lastname':
-                validate_length(element, 2)
+                check_length(element)
                 break;
             case 'email':
                 check_email(element)
@@ -153,10 +160,10 @@ form.addEventListener('submit', function(e) {
                 check_date(element)
                 break;
             case 'address':
-                validate_length(element, 2)
+                check_length(element)
                 break;
             case 'zip_code':
-                more_or_less(element, 5)
+                check_exact(element)
                 break;
         }
     });
